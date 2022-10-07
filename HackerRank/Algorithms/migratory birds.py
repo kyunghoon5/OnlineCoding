4fb605742e22ce9758e2603245b8705a7fcba1a1
KO mac <8754123@gmail.com>
2022-10-06 21:01:56 -0400

    fisrt commit

---
+++ HackerRank/Algorithms/migratory birds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    lst = []
+    i = 0
+    for i in range(6):
+        lst.append(arr.count(i))
+    return lst.index(max(lst))
+
+if __name__ == '__main__':
+    fptr = sys.stdout
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
